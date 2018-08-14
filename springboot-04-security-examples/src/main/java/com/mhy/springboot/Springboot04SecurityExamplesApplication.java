427078548db69ac75dd394eca1943f313a58a53d
mhy2011 <mhy2011@163.com>
2018-08-14 19:21:50 +0800

    add security module

---
+++ springboot-04-security-examples/src/main/java/com/mhy/springboot/Springboot04SecurityExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.mhy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04SecurityExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04SecurityExamplesApplication.class, args);
+	}
+}
