aec0a48eeb2ac96af02c22133213b63ab4520924
mhy2011 <mhy2011@163.com>
2018-08-15 22:34:48 +0800

    add WebSecurityConfig

---
+++ springboot-04-security-examples/src/main/java/com/mhy/springboot/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.mhy.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author mahaiyuan
+ * @ClassName: WebSecurityConfig
+ * @date 2018-08-14 下午7:26
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("wyf").password("wyf").roles("ROLE_ADMIN")
+                .and()
+                .withUser("wisely").password("wisely").roles("ROLE_USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
