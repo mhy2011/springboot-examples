c328fcd821285ca1a6ef1f3471df89d8d451c9c1
mhy2011 <mhy2011@163.com>
2018-09-07 20:28:17 +0800

    add mybatis examples

---
+++ springboot-07-mybatis-examples/src/main/java/com/mhy/springboot/Springboot07MybatisExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.mhy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot07MybatisExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot07MybatisExamplesApplication.class, args);
+	}
+}
